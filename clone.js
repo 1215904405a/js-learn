97230170299ebea2dd526f93bed9e39842c90e74
1215904405a <1215904405@qq.com>
2017-06-09 10:24:50 +0800

    Create clone.js

--- clone.js
+++ clone.js
@@ -2,3 +2,25 @@ function clone(origin) {  //克隆
   let originProto = Object.getPrototypeOf(origin);
   return Object.assign(Object.create(originProto), origin);
 }
+
+function clone(obj){
+  var buf;
+  if(obj instanceof Array){
+    buf = [];
+    var i = obj.length;
+    while(i--){
+      buf[i] = clone(obj[i]);
+    }
+    return buf;
+  }else if(obj instanceof Object){
+    buf = {};
+    for(var k in obj){
+      buf[k] = clone(obj[k]);
+    }
+    return buf;
+  }else{
+    return obj;
+  }
+}
+
+Object.getOwnPropertyNames(Object);//获取不可枚举属性
