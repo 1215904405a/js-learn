3a6c2ebf2667013d62e76ee481425de14345ed94
1215904405a <1215904405@qq.com>
2018-02-26 11:56:50 +0800

    Create js继承.js

---
+++ js继承.js
@@ -0,0 +1,26 @@
+//继承包括  原型继承   构造函数继承
+
+// Shape - 父类(superclass)
+function Shape() {
+  this.x = 0;
+  this.y = 0;
+}
+
+// 父类的方法
+Shape.prototype.move = function(x, y) {
+  this.x += x;
+  this.y += y;
+  console.info('Shape moved.');
+};
+
+// Rectangle - 子类(subclass)
+function Rectangle() {
+  Shape.call(this); // call super constructor.
+}
+
+// 子类续承父类
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+// 因为使用“.prototype =...”后,constructor会改变为“=...”的那个
+// constructor，所以要重新指定.constructor 为自身。
