b0a2e9be90eff21dd767440e9820356aa0f00750
1215904405a <1215904405@qq.com>
2018-09-12 21:18:41 +0800

    Update js数组去重.js

--- js数组去重.js
+++ js数组去重.js
@@ -1,1 +1,43 @@
+数组去重常用方法：
+
+1、对象：
+var array = [{value: 1}, {value: 1}, {value: 2}];
+
+function unique(array) {
+    var obj = {};
+    return array.filter(function(item, index, array){
+        console.log(typeof item + JSON.stringify(item))
+        return obj.hasOwnProperty(typeof item + JSON.stringify(item)) ? false : (obj[typeof item + JSON.stringify(item)] = true)
+    })
+}
+
+var array = [1, 2, 1, 1, '1'];
+
+function unique(array) {
+    var obj = {};
+    return array.filter(function(item, index, array){
+        return obj.hasOwnProperty(item) ? false : (obj[item] = true)
+    })
+}
+
+
+var array = [1, 2, 1, 1, '1'];
+
+function unique(array) {
+    var res = array.filter(function(item, index, array){
+        return array.indexOf(item) === index;
+    })
+    return res;
+}
+
+var array = [1, 2, 1, 1, '1'];
+
+function unique(array) {
+    return array.concat().sort().filter(function(item, index, array){
+        return !index || item !== array[index - 1]
+    })
+}
+
+
+
 参考： https://github.com/mqyqingfeng/Blog/issues/27
