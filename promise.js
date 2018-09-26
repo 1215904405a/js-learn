77353261886895b695823ba99029717270853a2c
1215904405a <1215904405@qq.com>
2018-09-26 20:27:17 +0800

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,3 +1,69 @@
+// resolve then
+function Promise(fn) {
+    var state = 'pending',
+        value = null,
+        callbacks = [];
+
+    this.then = function (onFulfilled) {   // 执行下本作用域对象的回调  并且返回下一次新的对象
+        return new Promise(function (resolve) {
+            handle({
+                onFulfilled: onFulfilled || null,
+                resolve: resolve
+            });
+        });
+    };
+
+    function handle(callback) { // 执行回调函数 或者执行新对象的resolve
+        if (state === 'pending') {
+            callbacks.push(callback);
+            return;
+        }
+        //如果then中没有传递任何东西
+        if(!callback.onFulfilled) {
+            callback.resolve(value);
+            return;
+        }
+
+        var ret = callback.onFulfilled(value);
+        callback.resolve(ret);
+    }
+
+    
+    function resolve(newValue) {   // 改变状态
+        if (newValue && (typeof newValue === 'object' || typeof newValue === 'function')) {
+            var then = newValue.then;
+            if (typeof then === 'function') {
+                then.call(newValue, resolve);
+                return;
+            }
+        }
+        state = 'fulfilled';
+        value = newValue;
+        setTimeout(function () {
+            callbacks.forEach(function (callback) {
+                handle(callback);
+            });
+        }, 0);
+    }
+
+    fn(resolve);
+}
+
+var p4 = new Promise(function(resolve,reject){
+	resolve(12);
+});
+
+p4.then(res=>{
+console.log(res);
+});
+
+
+
+
+
+
+
+
 简单模拟：
 let ref = (value) => {
     if (value && typeof value.then === "function")
