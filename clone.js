55cfb3f4bb289eb8677fe9a63e9eafed52ff55d4
1215904405a <1215904405@qq.com>
2018-09-27 11:14:16 +0800

    Update clone.js

--- clone.js
+++ clone.js
@@ -1,3 +1,50 @@
+Object.prototype.clone = function() { // 原型
+     // Handle null or undefined or function
+     if (null == this || "object" != typeof this)
+         return this;
+     // Handle the 3 simple types, Number and String and Boolean
+     if(this instanceof Number || this instanceof String || this instanceof Boolean)
+         return this.valueOf();
+     // Handle Date
+     if (this instanceof Date) {
+         var copy = new Date();
+         copy.setTime(this.getTime());
+         return copy;
+     }
+     // Handle Array or Object
+     if (this instanceof Object || this instanceof Array) {
+         var copy = (this instanceof Array)?[]:{};
+         for (var attr in this) {
+             if (this.hasOwnProperty(attr))
+                 copy[attr] = this[attr]?this[attr].clone():this[attr];
+         }
+         return copy;
+     }
+     throw new Error("Unable to clone obj! Its type isn't supported.");
+}
+
+function clone(obj) { // 对象
+     // Handle the 3 simple types, and null or undefined or function
+     if (null == obj || "object" != typeof obj) return obj;
+ 
+     // Handle Date
+     if (obj instanceof Date) {
+         var copy = new Date();
+         copy.setTime(obj.getTime());
+         return copy;
+     }
+     // Handle Array or Object
+     if (obj instanceof Array | obj instanceof Object) {
+         var copy = (obj instanceof Array)?[]:{};
+         for (var attr in obj) {
+             if (obj.hasOwnProperty(attr))
+               copy[attr] = clone(obj[attr]);
+         }
+         return copy;
+     }
+     throw new Error("Unable to clone obj! Its type isn't supported.");
+ }
+
 function clone(origin) {  //克隆
   let originProto = Object.getPrototypeOf(origin);
   return Object.assign(Object.create(originProto), origin);
